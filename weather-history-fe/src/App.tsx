00fd79c73b3bf7a9f066fbcec141af97b68e0998
Chris Charles <chris@creatively.co.uk>
2022-08-10 12:58:00 +0100

    FE React app created and simplified ready to start

---
+++ weather-history-fe/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+
+  
+  return (
+    <div className="App">
+      Hi from App.tsx App
+      <header className="App-header">Hi from App.tsx Header
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
